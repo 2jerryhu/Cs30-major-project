ff54d1383544cc39ecf58adbe50bc8a6d26bb66f
2jerryhu <jerryhu1995@gmail.com>
2023-04-28 15:19:04 -0600

    April 28th - display letter

--- sketch.js
+++ sketch.js
@@ -6,22 +6,37 @@
 // - describe what you did to take this project "above and beyond"
 // use p5play?
 
-class OneKey {
-  constructor(x, y, letter) {
+let frosty;
+
+class Letter {
+  constructor(x, y, array, index, prompt) {
     this.x = x;
     this.y = y;
-    this.letter = letter;
+    this.array = array;
+    this.index = index;
+    this.prompt = prompt;
     this.letterSize = 16;
     this.rgb = 140;
   }
 
   display() {
     fill(this.rgb);
-    
+    textSize(this.letterSize);
+    text(this.array[this.index], this.x, this.y);
+  }
+
+  update() {
+
   }
 }
 
+function preload() {
+  frosty = loadStrings("assets/forsty-the-snowman.txt");
+}
+
+
 let letters = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let thisKey = new Letter(200, 200, letters, 4);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -29,8 +44,10 @@ function setup() {
 
 function draw() {
   background(220);
-  push();
-  translate(mouseX, mouseY);
-  text("belloooooo", 300, 300);
-  pop();
+  thisKey.display();
+  for (let snowmans in frosty.length) {
+    
+    let letterNumber = 0;
+
+  }
 }
